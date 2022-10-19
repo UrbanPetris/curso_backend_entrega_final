a547d743cc1ea06e37aca6ca17b58bf09fc1a8e0
Pablo Ignacio Depetris Chauvin <pdepetrischauvin@gmail.com>
2022-10-19 10:32:42 -0300

    first commit

--- models/productos.js
+++ models/productos.js
@@ -4,10 +4,9 @@ const productosCollection = "productos";
 const productosSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  code: { type: String, required: true },
+  category: { type: String, required: true },
   photourl: { type: String, required: true },
   price: { type: Number, required: true },
-  stock: { type: Number, required: true },
   //agregar propiedades...
 });
 
